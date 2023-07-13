d0eddf2c7f528b942eb285232f00162caf539873
chinhnd7 <ndchinh110797@gmail.com>
2023-07-14 04:06:51 +0700

    html css exercise practice

---
+++ functions.js
@@ -0,0 +1,19 @@
+function goHomePage() {
+    window.location.href = "../homepage/index.html";
+}
+
+function goStorePage() {
+    window.location.href = "../store/index.html";
+}
+
+function goNewPage() {
+    window.location.href = "../new-1/index.html";
+}
+
+function goSupportPage() {
+    window.location.href = "../support-account-setting-1/index.html";
+}
+
+function goDetailPage() {
+    window.location.href = "../game-details-1/index.html";
+}
---
+++ open.bat
@@ -0,0 +1,2 @@
+@echo off
+start homepage/index.html
